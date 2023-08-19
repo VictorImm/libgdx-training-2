54e92eb75123a0633f9a60a5fd3b57b9194e0339
VictorImm <victorimmanuelss@gmail.com>
2023-08-20 00:36:24 +0700

    [UPDATE] change dir

---
+++ app/src/main/java/com/example/test_libgdx_2/model/SquareGDX.kt
@@ -0,0 +1,149 @@
+package com.example.test_libgdx_2.model
+
+import android.opengl.GLES20
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+import java.nio.FloatBuffer
+import java.nio.ShortBuffer
+
+const val COORDS_PER_VETEX = 3
+
+class SquareGDX(private val squareCoords: FloatArray) {
+
+    private val drawOrder = shortArrayOf(0, 1, 2, 0, 2, 3)
+
+    private val vertexShaderCode =
+    // This matrix member variable provides a hook to manipulate
+        // the coordinates of the objects that use this vertex shader
+        "uniform mat4 uMVPMatrix;" +
+                "attribute vec4 vPosition;" +
+                "void main() {" +
+                // the matrix must be included as a modifier of gl_Position
+                // Note that the uMVPMatrix factor *must be first* in order
+                // for the matrix multiplication product to be correct.
+                "  gl_Position = uMVPMatrix * vPosition;" +
+                "}"
+
+    // use to access and set the view transformation
+    private var vPMatrixHandle: Int = 0
+
+    private val fragmentShaderCode =
+        "precision mediump float;" +
+                "uniform vec4 vColor;" +
+                "void main() {" +
+                "  gl_FragColor = vColor;" +
+                "}"
+
+    private fun loadShader(type: Int, shaderCode: String): Int {
+
+        // create a vertex shader type (GLES20.GL_VERTEX_SHADER)
+        // or a fragment shader type (GLES20.GL_FRAGMENT_SHADER)
+        return GLES20.glCreateShader(type).also { shader ->
+
+            // add the source code to the shader and compile it
+            GLES20.glShaderSource(shader, shaderCode)
+            GLES20.glCompileShader(shader)
+        }
+    }
+
+    // set color with RGBA values
+    private val color = floatArrayOf(0.8f, 0.3f, 0.22265625f, 0.4f)
+
+    private var vertexBuffer: FloatBuffer =
+        // (number of coordinate values * 4 bytes per float)
+        ByteBuffer.allocateDirect(squareCoords.size*4).run {
+            // use the device hardware's native byte order
+            order(ByteOrder.nativeOrder())
+
+            // create a floating point buffer from the ByteBuffer
+            asFloatBuffer().apply {
+                // add the coordinates to the FloatBuffer
+                put(squareCoords)
+                // set the buffer to read the first coordinate
+                position(0)
+            }
+        }
+
+    // initialize byte buffer for the draw list
+    private val drawListBuffer: ShortBuffer =
+        // (number of coordinate values * 2 bytes per short)
+        ByteBuffer.allocateDirect(drawOrder.size * 2).run {
+            order(ByteOrder.nativeOrder())
+            asShortBuffer().apply {
+                put(drawOrder)
+                position(0)
+            }
+        }
+
+    private var mProgram: Int
+
+    init {
+        val vertexShader: Int = loadShader(GLES20.GL_VERTEX_SHADER, vertexShaderCode)
+        val fragmentShader: Int = loadShader(GLES20.GL_FRAGMENT_SHADER, fragmentShaderCode)
+
+        // create empty OpenGL ES Program
+        mProgram = GLES20.glCreateProgram().also {
+
+            // add the vertex shader to program
+            GLES20.glAttachShader(it, vertexShader)
+
+            // add the fragment shader to program
+            GLES20.glAttachShader(it, fragmentShader)
+
+            // creates OpenGL ES program executables
+            GLES20.glLinkProgram(it)
+        }
+    }
+
+    private var positionHandle: Int = 0
+    private var mColorHandle: Int = 0
+
+    private val vertexCount: Int = squareCoords.size / COORDS_PER_VERTEX
+    private val vertexStride: Int = COORDS_PER_VERTEX * 4 // 4 bytes per vertex
+
+    fun draw(mvpMatrix: FloatArray) {
+        // Add program to OpenGL ES environment
+        GLES20.glUseProgram(mProgram)
+
+        // get handle to shape's transformation matrix
+        vPMatrixHandle = GLES20.glGetUniformLocation(mProgram, "uMVPMatrix")
+
+        // Pass the projection and view transformation to the shader
+        GLES20.glUniformMatrix4fv(vPMatrixHandle, 1, false, mvpMatrix, 0)
+
+        // get handle to vertex shader's vPosition member
+        positionHandle = GLES20.glGetAttribLocation(mProgram, "vPosition").also {
+
+            // Enable a handle to the square vertices
+            GLES20.glEnableVertexAttribArray(it)
+
+            // Prepare the square coordinate data
+            GLES20.glVertexAttribPointer(
+                it,
+                COORDS_PER_VERTEX,
+                GLES20.GL_FLOAT,
+                false,
+                vertexStride,
+                vertexBuffer
+            )
+
+            // get handle to fragment shader's vColor member
+            mColorHandle = GLES20.glGetUniformLocation(mProgram, "vColor").also { colorHandle ->
+
+                // Set color for drawing the square
+                GLES20.glUniform4fv(colorHandle, 1, color, 0)
+            }
+
+            // Draw the square
+            GLES20.glDrawElements(
+                GLES20.GL_TRIANGLES,
+                drawOrder.size,
+                GLES20.GL_UNSIGNED_SHORT,
+                drawListBuffer)
+
+            // Disable vertex array
+            GLES20.glDisableVertexAttribArray(positionHandle)
+        }
+    }
+
+}
